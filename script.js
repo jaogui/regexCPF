1a0210c011f741419429f703bedde81f28adbe23
João Benine <joaobenine@gmail.com>
2022-11-27 18:38:02 -0300

    html para validar cpf

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemsCPF = document.querySelectorAll(".list-cpf li");
 // console.log(itemsCPF);
 
 
+
 //Formata CPF
 //Desestrutura elementos transformando em array.
 //Map em cada elemento obtendo o texto
@@ -9,11 +10,13 @@ function captureText([...elements]) {
     return elements.map((element) => element.innerText);
 }
 
+
 //Limpa CPF
 //Limpa todos caracteres dos números
 const cleanCPF = (cpf) => {
     return cpf.replace(/\D/g, "");
 };
+
 
 //Formata CPF
 //Captura sequência de 3 números e por último de 2 números e substitui com o parâmetro definido em replace
@@ -21,12 +24,12 @@ const formatCPF = (cpf) => {
     return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
 };
 
+
 //BuildCPF
 //Executa aconstrução do cpf.
 const buildCPFS = (cpfs) => {
     return cpfs.map(cleanCPF).map(formatCPF);
 };
-
 
 const insertCPF = (listaCPF) => {
     const cpfs = captureText(listaCPF);
