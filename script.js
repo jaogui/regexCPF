daa5c7472b089e081d3dcac2a5ed7d503878744d
João Benine <joaobenine@gmail.com>
2022-11-27 17:44:19 -0300

    limpando cpf com regex

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-//Formata CPF
-
-
 const itemsCPF = document.querySelectorAll('.list-cpf li');
 console.log(itemsCPF);
 
-
+//Formata CPF
+//Desestrutura elementos transformando em array.
+//Map em cada elemento obtendo o texto
 function captureText([...elements]) {
     return elements.map(element => element.innerText)
 }
 
 
-console.log(captureText(itemsCPF));
+//Limpa caracteres do CPF
+const cleanCPF = (cpf) => {
+    return cpf.replace(/[ .-]/g, '')
+
+}
+
+
+console.log(cleanCPF(('156.457.444 10')))
+
+console.log(captureText(itemsCPF))
